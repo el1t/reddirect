6e77edf0287c3cf91145216c5a0c5e6441c0d710
Ellis Tsung <git@tsung.co>
2019-09-08 17:12:27 -0700

    Conform to design guidelines
    - Quit app after closing window
    - Show close button
    - Fix Safari button

--- reddirect/ViewController.swift
+++ reddirect/ViewController.swift
@@ -11,13 +11,6 @@ import SafariServices.SFSafariApplication
 
 class ViewController: NSViewController {
 
-    @IBOutlet var appNameLabel: NSTextField!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        appNameLabel.stringValue = "One-liner script that redirects www.reddit.com to old.reddit.com"
-    }
-
 	@IBAction func openGithub(_ sender: AnyObject?) {
 		guard let url = URL(string: "https://github.com/el1t/reddirect") else { return }
 		NSWorkspace.shared.open(
@@ -28,9 +21,9 @@ class ViewController: NSViewController {
 			launchIdentifiers: nil
 		)
 	}
-    
+
     @IBAction func openSafariExtensionPreferences(_ sender: AnyObject?) {
-        SFSafariApplication.showPreferencesForExtension(withIdentifier: "co.tsung.reddirect-Extension") { error in
+        SFSafariApplication.showPreferencesForExtension(withIdentifier: "co.tsung.reddirect.extension") { error in
             if let _ = error {
                 // Insert code to inform the user that something went wrong.
 
